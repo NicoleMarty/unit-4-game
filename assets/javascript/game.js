90a7bf784ec9c2d88995b3e09debdd691d112967
Nicole Marty <nmarty310@gmail.com>
2019-07-22 18:03:59 -0500

    fixed it so the character selected stays at the top and can still select different character after refreshing the page.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,50 +1,55 @@
 // When the game starts, 
 $(document).ready(function() {
 
+    var playerChoice = ["player1", "player2", "player3", "player4"];
 
     // Pick a player and move remainder to "Your Enemies"
+
     if ($("#player1>button").on("click", function() {
             $(this).toggleClass("player-button");
-            var player2Button = $('#player2>button').detach();
+            var player2Button = $('#player2>button').remove();
             player2Button.appendTo('#enemies');
-            var player3Button = $('#player3>button').detach();
+            var player3Button = $('#player3>button').remove();
             player3Button.appendTo('#enemies');
-            var player4Button = $('#player4>button').detach();
+            var player4Button = $('#player4>button').remove();
             player4Button.appendTo('#enemies');
 
         }));
 
-    else if ($("#player2>button").on("click", function() {
+    if ($("#player2>button").on("click", function() {
             $(this).toggleClass("player-button");
-            var player1Button = $('#player1>button').detach();
+            var player1Button = $('#player1>button').remove();
             player1Button.appendTo('#enemies');
-            var player3Button = $('#player3>button').detach();
+            var player3Button = $('#player3>button').remove();
             player3Button.appendTo('#enemies');
-            var player4Button = $('#player4>button').detach();
+            var player4Button = $('#player4>button').remove();
             player4Button.appendTo('#enemies');
         }));
 
-    else if ($("#player3>button").on("click", function() {
+    if ($("#player3>button").on("click", function() {
             $(this).toggleClass("player-button");
-            var player1Button = $('#player1>button').detach();
+            var player1Button = $('#player1>button').remove();
             player1Button.appendTo('#enemies');
-            var player2Button = $('#player2>button').detach();
+            var player2Button = $('#player2>button').remove();
             player2Button.appendTo('#enemies');
-            var player4Button = $('#player4>button').detach();
+            var player4Button = $('#player4>button').remove();
             player4Button.appendTo('#enemies');
         }));
 
-    else($("#player4>button").on("click", function() {
-        $(this).toggleClass("player-button");
-        var player1Button = $('#player1>button').detach();
-        player1Button.appendTo('#enemies');
-        var player2Button = $('#player2>button').detach();
-        player2Button.appendTo('#enemies');
-        var player3Button = $('#player3>button').detach();
-        player3Button.appendTo('#enemies');
-    }));
+    if ($("#player4>button").on("click", function() {
+            $(this).toggleClass("player-button");
+            var player1Button = $('#player1>button').remove();
+            player1Button.appendTo('#enemies');
+            var player2Button = $('#player2>button').remove();
+            player2Button.appendTo('#enemies');
+            var player3Button = $('#player3>button').remove();
+            player3Button.appendTo('#enemies');
+        }));
+
+
+
+
 
 
 
 });
-e
