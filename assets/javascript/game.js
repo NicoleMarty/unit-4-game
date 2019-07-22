43c81a7dbf776019afc59b50aa315af786675854
Nicole Marty <nmarty310@gmail.com>
2019-07-22 17:25:56 -0500

    converted if statements to if, else if, else if, else to make sure your player stays your player for the remainder of the game.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,8 @@
 // When the game starts, 
 $(document).ready(function() {
 
+
+    // Pick a player and move remainder to "Your Enemies"
     if ($("#player1>button").on("click", function() {
             $(this).toggleClass("player-button");
             var player2Button = $('#player2>button').detach();
@@ -9,12 +11,40 @@ $(document).ready(function() {
             player3Button.appendTo('#enemies');
             var player4Button = $('#player4>button').detach();
             player4Button.appendTo('#enemies');
+
         }));
 
-    $("#player2>button").on("click", function() { $(this).toggleClass("player-button"); });
-    $("#player3>button").on("click", function() { $(this).toggleClass("player-button"); });
-    $("#player4>button").on("click", function() { $(this).toggleClass("player-button"); });
+    else if ($("#player2>button").on("click", function() {
+            $(this).toggleClass("player-button");
+            var player1Button = $('#player1>button').detach();
+            player1Button.appendTo('#enemies');
+            var player3Button = $('#player3>button').detach();
+            player3Button.appendTo('#enemies');
+            var player4Button = $('#player4>button').detach();
+            player4Button.appendTo('#enemies');
+        }));
+
+    else if ($("#player3>button").on("click", function() {
+            $(this).toggleClass("player-button");
+            var player1Button = $('#player1>button').detach();
+            player1Button.appendTo('#enemies');
+            var player2Button = $('#player2>button').detach();
+            player2Button.appendTo('#enemies');
+            var player4Button = $('#player4>button').detach();
+            player4Button.appendTo('#enemies');
+        }));
+
+    else($("#player4>button").on("click", function() {
+        $(this).toggleClass("player-button");
+        var player1Button = $('#player1>button').detach();
+        player1Button.appendTo('#enemies');
+        var player2Button = $('#player2>button').detach();
+        player2Button.appendTo('#enemies');
+        var player3Button = $('#player3>button').detach();
+        player3Button.appendTo('#enemies');
+    }));
 
 
 
 });
+e
