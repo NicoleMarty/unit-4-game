7fb91ae6830db500d64168b97043127aded84199
Nicole Marty <nmarty310@gmail.com>
2019-07-23 12:05:50 -0500

    redirect the appendTo to specific #s within the enemies section.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,43 +8,44 @@ $(document).ready(function() {
     if ($("#player1>button").on("click", function() {
             $(this).toggleClass("player-button");
             var player2Button = $('#player2>button').remove();
-            player2Button.appendTo('#enemies');
+            player2Button.appendTo('#opponent1');
             var player3Button = $('#player3>button').remove();
-            player3Button.appendTo('#enemies');
+            player3Button.appendTo('#opponent2');
             var player4Button = $('#player4>button').remove();
-            player4Button.appendTo('#enemies');
+            player4Button.appendTo('#opponent3');
 
         }));
 
     if ($("#player2>button").on("click", function() {
             $(this).toggleClass("player-button");
             var player1Button = $('#player1>button').remove();
-            player1Button.appendTo('#enemies');
+            player1Button.appendTo('#opponent1');
             var player3Button = $('#player3>button').remove();
-            player3Button.appendTo('#enemies');
+            player3Button.appendTo('#opponent2');
             var player4Button = $('#player4>button').remove();
-            player4Button.appendTo('#enemies');
+            player4Button.appendTo('#opponent3');
         }));
 
     if ($("#player3>button").on("click", function() {
             $(this).toggleClass("player-button");
             var player1Button = $('#player1>button').remove();
-            player1Button.appendTo('#enemies');
+            player1Button.appendTo('#opponent1');
             var player2Button = $('#player2>button').remove();
-            player2Button.appendTo('#enemies');
+            player2Button.appendTo('#opponent2');
             var player4Button = $('#player4>button').remove();
-            player4Button.appendTo('#enemies');
+            player4Button.appendTo('#opponent3');
         }));
 
     if ($("#player4>button").on("click", function() {
             $(this).toggleClass("player-button");
             var player1Button = $('#player1>button').remove();
-            player1Button.appendTo('#enemies');
+            player1Button.appendTo('#opponent1');
             var player2Button = $('#player2>button').remove();
-            player2Button.appendTo('#enemies');
+            player2Button.appendTo('#opponent2');
             var player3Button = $('#player3>button').remove();
-            player3Button.appendTo('#enemies');
+            player3Button.appendTo('#opponent3');
         }));
+
 
 
 
