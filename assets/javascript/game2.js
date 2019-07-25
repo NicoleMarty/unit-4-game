becaf1d0316353bfb2c2b2e59f244ae33efa3e61
Nicole Marty <nmarty310@gmail.com>
2019-07-25 15:12:20 -0500

    added in font from google fonts, bg image, and created characters in js

---
+++ assets/javascript/game2.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    var characters = {
+        "Go-Go": {
+            name: "Go-Go",
+            health: 120,
+            attack: 8,
+            imageUrl: "assets/images/go-go-crazy.jpg",
+            enemyAttackBack: 15
+        },
+        "Purge": {
+            name: "Purge",
+            health: 100,
+            attack: 14,
+            imageUrl: "assets/images/purge-dude.jpg",
+            enemyAttackBack: 5
+        },
+        "Blue Ice": {
+            name: "Blue Ice",
+            health: 150,
+            attack: 8,
+            imageUrl: "assets/images/blue-ice.jpg",
+            enemyAttackBack: 5
+        },
+        "Pantera": {
+            name: "Pantera",
+            health: 180,
+            attack: 7,
+            imageUrl: "assets/images/robo-panther.jpg",
+            enemyAttackBack: 25
+        },
+    };
+});
